3061e0109d53f17cbaa8cad98d7ebb25de260f33
Mark Middleton <mark@zaltek.co.uk>
2023-10-24 17:42:32 +0100

    wip

--- custom-addons/zaltek/__manifest__.py
+++ custom-addons/zaltek/__manifest__.py
@@ -12,14 +12,14 @@
     'author': "Zaltek Digital",
     'website': "https://zaltek.co.uk",
 
-    # Categories can be used to filter modules in modules listing
+    # Categories can datbe used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'web'],
 
     # always loaded
     'data': [
--- custom-addons/zaltek/controllers/controllers.py
+++ custom-addons/zaltek/controllers/controllers.py
@@ -1,28 +1,25 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 from odoo.http import request
-
+from werkzeug.exceptions import Forbidden, NotFound
 from odoo.addons.web.controllers.database import Database
-
-
 
 class ZaltekController(Database):
 
     @http.route('/web/database/manager', type='http', auth="none")
     def manager(self, **kw):
-        # context = self._context
-
         uid = request.env.context.get('uid')
         if( not uid ) :
-            return http.request.render('website.page_404')
+            raise Forbidden()  
+            # return http.request.render('website.page_404')
         
         user = request.env['res.users'].browse(uid)
 
         if( not user ) :
-            return http.request.render('website.page_404')
+            raise Forbidden()  
 
         if( not user._is_admin() ) :
-            return http.request.render('website.page_404')
+            raise Forbidden()
         
         return super(ZaltekController, self).manager()
 
