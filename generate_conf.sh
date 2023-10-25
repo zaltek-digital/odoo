4889ae6cba169f3f8cd1abac15d50ef2e4402d1a
Mark Middleton <mark@zaltek.co.uk>
2023-10-25 13:01:54 +0100

    ploi

--- generate_conf.sh
+++ generate_conf.sh
@@ -2,5 +2,9 @@
 cp .odoo.conf .odoo1.conf
 cat .env | sed  's/###//g' > .odoo.conf
 echo '\n' >> .odoo.conf
+if [ ! -f ".odoo1.conf" ]; then
+  touch .odoo1.conf
+fi
+
 cat .odoo1.conf | sed -e '1,/#END/ d' | sed '/^$/d' >> .odoo.conf 
 cat .odoo.conf
